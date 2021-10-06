01ac9d9e3fe86225cd649d578e5ad2fb4422ec9a
serge-thui <serge74.thuillier@gmail.com>
2021-10-06 15:42:32 +0200

    slider avec photo

---
+++ js/main.js
@@ -0,0 +1,33 @@
+/*js du slider*/
+const section=document.querySelector('.slider');
+const items=section.querySelectorAll('img');
+const nbSlide=items.length;
+const suivant=document.querySelector('button.btn-nav-right');
+const precedent=document.querySelector('button.btn-nav-left');
+let count=0;
+
+function slideSuivante(){
+    items[count].classList.remove('active');
+
+    if(count<nbSlide-1){
+        count++;
+    }else{
+        count=0;
+    }
+
+    items[count].classList.add('active');
+}
+suivant.addEventListener('click', slideSuivante);
+
+function slidePrecedente(){
+    items[count].classList.remove('active');
+
+    if(count>0){
+        count--;
+    }else{
+        count=nbSlide-1;
+    }
+
+    items[count].classList.add('active');
+}
+precedent.addEventListener('click', slidePrecedente);
